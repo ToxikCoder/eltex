1b2e39daa7c40a496b3d5f757b01e5d95c321409
user <user@debvirtualenv.home.virtual.workstation>
2016-10-09 21:40:55 +0700

    Lab2[2/3] Sorting and swapping functions

--- lab2/sort.c
+++ lab2/sort.c
@@ -2,6 +2,7 @@
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
+#include "stdbool.h"
 
 int inp_str(char* string, int maxlen)
 {
@@ -22,19 +23,62 @@ void out_str(char* string, int length, int number)
 	
 }
 
+void swap(char* i, char* j)
+{
+	int temp = *i;
+	*i = *j;
+	*j = temp;
+}
+
 int sort(char* string, int size, enum SORT_ORDER order)
 {
-	return -1;
+	int shuffles = 0;
+	if(string == NULL || size <= 1) return -1;
+	for(int i = 0; i < size - 2; ++i)
+	{
+		bool swapped = false;
+		for(int j = 0; j < size - i - 2; ++j)
+		{
+			if(order == ASCENDING)
+			{
+				if(string[j] > string[j+1])
+				{
+					swap(&string[j], &string[j+1]);
+					swapped = true;
+					++shuffles;
+				}
+			}
+			else
+			{
+				if(string[j] < string[j+1])
+				{
+					swap(&string[j], &string[j+1]);
+					swapped = true;
+					++shuffles;
+				}
+			}
+		}
+		if(!swapped)
+			break;
+	}
+	return shuffles;
 }
 
 int main()
 {
 	char string[MAXLEN];
 	int result = inp_str(string, MAXLEN);
-	if(result <= 0)
-		printf("Error\n");
-	else
-		printf("%d symbols read: %s\n", result, string);
+	if(result > 0)
+	{
+		printf("%d symbols read: %s", result, string);
+		result = sort(string, result, ASCENDING);
+		if(result > 0)
+		{
+			//sorted successfully
+			printf("%d reshuffles, resulting string: %s", result, string);
+
+		}
+	}
 
 	return 0;
 }
