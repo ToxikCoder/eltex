8fcecfd1ae8549cde26bd7db9598a8e0a0725471
Lev Sizov <levsizov2015@outlook.com>
2016-12-08 15:19:37 +0700

    Lab6 ready

--- lab6/lab.c
+++ lab6/lab.c
@@ -5,27 +5,33 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <limits.h>
 
 #define NUM_WORKERS 4
 
-int capacity = 100;
 int performance = 4;
 
 void work()
 {
+	srand((unsigned)getpid());
 	pid_t  cur_pid = getpid();
 	int count = 0;
+	float sleeptime = 0.0f;
+	int capacity = rand() % 100;
 	printf("Worker #%d is going to mine...\n", cur_pid);
+	fflush(stdout);
 	while(true)
 	{
 		if(capacity <= 0)
 		{
-			printf("Nothing to mine, going home\nWorker #%d visited mine %d times\n", cur_pid, count);
+			printf("Nothing to mine, going home\nWorker #%d visited mine %d times in %f seconds\n", cur_pid, count, sleeptime);
+			fflush(stdout);
 			exit(0);
 		}
 		++count;
 		capacity -= performance;
-		sleep((unsigned)rand() % 5);
+		sleeptime += rand() % 5 - 1;
+		sleep((unsigned)1);
 	}
 }
 
@@ -33,9 +39,10 @@ int main()
 {
 	srand((unsigned)getpid());
 	pid_t pid;
-	printf("Mine capacity: %d\nNumber of workers: %d\nSending them to mine...\n", capacity, NUM_WORKERS);
+	printf("Number of workers: %d\nSending them to mine...\n",  NUM_WORKERS);
 	for(int i = 0; i < NUM_WORKERS; ++i)
 	{
+		fflush(stdout);
 		pid = fork();
 		if(pid == 0) work();
 	}
