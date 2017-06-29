1352008c02fc313dc57f1f5f7b5415fbed3dcb14
Lev Sizov <levsizov2015@outlook.com>
2017-06-29 01:32:51 -0700

    Lab 8 - finished

--- lab8/lab.c
+++ lab8/lab.c
@@ -15,7 +15,7 @@
 #define PERFORMANCE 4
 
 int main(void);
-void work(int);
+void work(int, int);
 void control(int);
 void check_error(int);
 
@@ -26,51 +26,48 @@ struct msgbuf
 };
 
 pid_t children[NUM_WORKERS];
+int visit_counter[NUM_WORKERS];
+
+/* Message types:
+ * 1-4 - message from woker, text=1 - work cycle
+ * 10+<child_type> - message from base for <child_type> child
+ *   text=1 - work processed, worker should continue
+ *   text=2 - mine empty, workers should finish working
+ */
 
 void check_error(int r)
 {
     if(r == -1 && errno != ENOMSG)
     {
         perror("Error: ");
-        fflush(stdout);
+        fflush(stderr);
         exit(-1);
     }
 }
 
-void work(int msqid)
+void work(int msqid, int message_type)
 {
-    int count = 0; //mine visits counter
-	int sleeptime = 0; //sleep time
     pid_t this_worker = getpid();
-    fprintf(stdout, "Worker #%d is going to mine\n", this_worker);
-    fflush(stdout);
     struct msgbuf rec; //for receiving messages
     struct msgbuf message; //for sending messages
-    message.mtype = this_worker;
+    message.mtype = message_type;
     message.mtext[0] = 1;
-    sleep(3);
     bool first = true;
     while(true)
     {
-        ssize_t size = msgrcv(msqid, &rec, 1, this_worker, IPC_NOWAIT);
+        ssize_t size = msgrcv(msqid, &rec, 1, 10 + message_type, IPC_NOWAIT);
         check_error(size);
-        if(size == 1 && rec.mtext[0] == 0)
+        if(size == 1 && rec.mtext[0] == 2)
             break; //0 - mine is empty, stop working
-        if(first || rec.mtext[0] == 2)
+        if(first || (size == 1 && rec.mtext[0] == 1))
         {
-            //base is processed last delivery, continue mining
+            //base processed last delivery, continue mining
             first = false;
             int result = msgsnd(msqid, &message, 1, 0);
             check_error(result);
-            ++count;
-            int sl = rand() % 2;
-            sleeptime += sl;
-            sleep((unsigned) sleeptime);
         }
     }
-    fprintf(stdout, "Mine is empty, worker #%d is going home\n", this_worker);
-    fprintf(stdout, "Worker %d visited mine %d times and sleeped for %d\n",
-        this_worker, count, sleeptime);
+    fprintf(stdout, "Worker #%d: Mine is empty, going home\n", this_worker);
     fflush(stdout);
     exit(0);
 }
@@ -82,43 +79,52 @@ void control(int msqid)
     bool mine_empty = false;
     while(true)
     {
-        for(short i = 0; i < NUM_WORKERS; ++i)
+        for(short i = 1; i <= NUM_WORKERS; ++i)
         {
-            ssize_t r = msgrcv(msqid, &message, 1, children[i], IPC_NOWAIT);
-            check_error(r);
-
-            if(message.mtext[0] != 1)
+            ssize_t size = msgrcv(msqid, &message, 1, i, IPC_NOWAIT);
+            check_error(size);
+            if(size == 1 && message.mtext[0] != 1)
                 continue;
 
-            if(capacity != 0)
+            if(capacity > PERFORMANCE)
             {
-                message.mtype = children[i];
-                message.mtext[0] = 2;
+                message.mtype = 10 + i;
+                message.mtext[0] = 1;
                 int r = msgsnd(msqid, &message, 1, 0);
                 check_error(r);
                 capacity -= PERFORMANCE;
-                fprintf(stdout,
-                    "Mine capacity: %d, worker #%d extracted %d of gold\n",
-                    capacity, children[i], PERFORMANCE);
-                fflush(stdout);
             }
-            else
+            else if(capacity <= PERFORMANCE)
             {
+                capacity = 0;
                 mine_empty = true;
-                break;
             }
+            visit_counter[i-1] += 1;
+            fprintf(stdout,
+                "Base: Mine capacity: %d, worker #%d extracted %d gold\n",
+                capacity, children[i-1], PERFORMANCE);
+            fflush(stdout);
+            if(mine_empty)
+                break;
         }
         if(mine_empty)
         {
-            message.mtext[0] = 0;
-            for(short i = 0; i < NUM_WORKERS; ++i)
+            fprintf(stdout, "Base: Sending workers home...\n");
+            fflush(stdout);
+            message.mtext[0] = 2;
+            for(short i = 1; i <= NUM_WORKERS; ++i)
             {
-                message.mtype = children[i];
+                message.mtype = 10 + i;
                 int result = msgsnd(msqid, &message, 1, 0);
                 check_error(result);
             }
             break;
         }
+    }
+    for(short i = 0; i < NUM_WORKERS; ++i)
+    {
+        fprintf(stdout, "Base: Worker #%d visited mine %d times\n",
+            children[i], visit_counter[i]);
     }
 }
 
@@ -128,19 +134,30 @@ int main(void)
     int queue = msgget(key, IPC_CREAT | 00660);
     check_error(queue);
     pid_t child_pid;
-    for(short i = 0; i < NUM_WORKERS; ++i)
+    for(short i = 1; i <= NUM_WORKERS; ++i)
     {
         child_pid = fork();
         //child process
         if(child_pid == 0)
         {
-            work(queue);
+            work(queue, i);
+        }
+        else if(child_pid < 0)
+        {
+            perror("Failed to fork process: ");
+            fflush(stderr);
+            exit(-1);
         }
         else
-            children[i] = child_pid;
+            children[i-1] = child_pid;
     }
     control(queue);
-    sleep(2);
+    for(short i = 0; i < NUM_WORKERS; ++i)
+    {
+        waitpid(children[i], NULL, 0);
+        fprintf(stdout, "Base: Worker #%d returned to base\n", children[i]);
+        fflush(stdout);
+    }
     int r = msgctl(queue, IPC_RMID, 0);
     check_error(r);
     return 0;
