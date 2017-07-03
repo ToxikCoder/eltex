c90624797004fdd8a48da0756b7f7e57871d556f
Lev Sizov <levsizov2015@outlook.com>
2017-07-03 08:40:00 +0100

    Protocol description, comments for later coding

--- lab11/lab.c
+++ lab11/lab.c
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <wait.h>
 
 /*
  * 1 - fork 4 clients
@@ -19,14 +20,27 @@
  * 8 - close listening socket
  */
 
+/*
+ * Client-server protocol
+ *
+ * Client2server: type = 0, message = <capacity>, 0 <= capacity < 128
+ * Server2client: type = 1, message = 1 - work is processed, worker can continue
+ */
+
 #define NUM_WORKERS 4
 #define PERFORMANCE 4
+
+struct packet
+{
+    short type : 1;
+    short message : 7;
+};
 
 struct resource
 {
     pthread_mutex_t capacity_mutex;
     int capacity;
-} mine = { PHTREAD_MUTEX_INITIALIZER, 200 };
+} mine = { PTHREAD_MUTEX_INITIALIZER, 200 };
 
 void client(void); //client process
 void work(int); //sends to server amount of gold extracted
@@ -34,14 +48,35 @@ void server(void); //server process
 void* job(void*); //server-side job working with clients
 int main(void);
 
+pid_t children[NUM_WORKERS];
+
 void client()
 {
+    //connecting to server
 
+    //starting working
+
+    //sending work done
+
+    //waiting for confirmation
+
+    exit(0);
 }
 
 void server()
 {
+    //listening to connections
 
+    //starting thread for every connection
+
+}
+
+void* job(void* arg)
+{
+    //waiting for packet from client
+
+    //reading amount of extracted gold and sending confirmation
+    
 }
 
 int main()
@@ -59,6 +94,10 @@ int main()
         else if(child_pid == 0)
         {
             client();
+        }
+        else
+        {
+            children[i] = child_pid;
         }
     }
     server();
